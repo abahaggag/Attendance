b3119b2701812b3656f1efbebdc9330212099421
abahaggag <bahajaj135@hotmail.com>
2016-04-12 19:30:06 +0530

    Change logDate format to dd/MM/yyyy

--- JISR_V1/frmAttendance.cs
+++ JISR_V1/frmAttendance.cs
@@ -350,7 +350,7 @@ namespace JISR_V1
                     new Record
                     {
                         id = Convert.ToString(row["EmployeeCode"]),
-                        day = Convert.ToDateTime(row["LogDate"]).ToString("MM/dd/yyyy"),
+                        day = Convert.ToDateTime(row["LogDate"]).ToString("dd/MM/yyyy"),
                         time = Convert.ToDateTime(row["LogDate"]).ToString("HH:mm"),
                         direction = row["Direction"].ToString()
                     }
